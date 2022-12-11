dc6928456c3ef649894c6b034049dccc2d987c8e
HamzaNajiG <hamzaasp@hotmail.com>
2022-12-12 01:28:54 +0200

    AWayTooLongWords

--- Program.cs
+++ Program.cs
@@ -1,26 +1,11 @@
 ﻿
 using CompProgramming;
 
-var w = Int32.Parse(System.Console.ReadLine());
- Watermelon.GetResult(w);
-
- public class Watermelon {
-
-        public static void GetResult(int w){
-            if (w<=100 && w >=1)
-            {
-            if (w%2 ==0 && w >3)
-            {
-                System.Console.WriteLine("YES");
-            }
-            
-            else{
-                System.Console.WriteLine("No");
-            } 
-                
-            }else
-            {
-                System.Console.WriteLine("No");
-            }
-        }
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var w = int.Parse(Console.ReadLine());
+        Watermelon.GetResult(w);
     }
+}
