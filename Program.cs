f0e79577aefe659497dfbddc87438ace3c34a314
HamzaNajiG <hamzaasp@hotmail.com>
2022-12-12 01:20:47 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using CompProgramming;
+
+var w = Int32.Parse(System.Console.ReadLine());
+ Watermelon.GetResult(w);
+
+ public class Watermelon {
+
+        public static void GetResult(int w){
+            if (w<=100 && w >=1)
+            {
+            if (w%2 ==0 && w >3)
+            {
+                System.Console.WriteLine("YES");
+            }
+            
+            else{
+                System.Console.WriteLine("No");
+            } 
+                
+            }else
+            {
+                System.Console.WriteLine("No");
+            }
+        }
+    }
---
+++ Watermelon.cs
@@ -0,0 +1,14 @@
+namespace CompProgramming
+{
+    public class Watermelon {
+
+        public static void GetResult(int w){
+            if (w%2 ==0 || w !=2)
+            {
+                System.Console.WriteLine("YES");
+            }else{
+                System.Console.WriteLine("No");
+            }
+        }
+    }
+}
